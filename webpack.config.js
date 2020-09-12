28d166dfb43fe4333881d3e4502bc270c258b8b0
“Becca” <r.e.burten@gmail.com>
2020-09-11 20:49:02 -0400

    Setup with Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+// copy and paste!
+module.exports = {
+    context: __dirname,
+    entry: './frontend/entry.jsx',
+    output: {
+        path: 'app/assets/javascripts', //path: path.resolve(__dirname, ''),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
