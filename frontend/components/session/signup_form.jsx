ad931aa03f4158e5267609dfa42f37cbcaa7cd19
“Becca” <r.e.burten@gmail.com>
2020-09-22 15:11:08 -0400

    Functioning Signup page e-mail error flag

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -5,40 +5,39 @@ import React from "react";
 class SignupForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { signup: { email: '', fname: '', lname: '', password: '' }, error: null };
+        this.state = { email: '', fname: '', lname: '', password: '' };
         this.handleClick = this.handleClick.bind(this);
         // this.confirmEmail = this.confirmEmail.bind(this);
     }
 
     handleClick(e) {
-        this.props.signup(this.state.signup);
+        this.props.signup(this.state);
     }
 
-    // confirmEmail(e) {
-    //     this.setState({signup: { email: e.target.value }});
-    //     if (this.state.signup.email !== this.props.email) {
-    //         this.state.error = [`Email address doesn't match. Please try again`]
-    //     };
-    // }
+    confirmEmail() {
+        if (this.state.email.length < 2) return null;
+        if (this.state.email !== this.props.email) {
+            return `Email address doesn't match. Please try again`;
+        } else {
+            return null
+        }
+    }
 
+    validPW() {
+        if (this.state.password.length < 8) {
+            return `Password must be at least 8 characters.`;
+        } else {
+            return null
+        }
+    }
     update(field){
-        // if (field === 'password') {
-        //     return (e) => {
-        //         let input = e.target.value
-        //         this.setState({ signup: { [field]: input } })
-        //         if (input.length < 8) {
-        //             this.props.errors = ['Password must be at least 8 characters.']
-        //         }
-        //     }
-        // } else {
-            return (e) => (
-                this.setState({signup: {[field]: e.target.value} })
-            )
-        // }
+        return (e) => (
+            this.setState({[field]: e.target.value})
+        )
     }
 
     render() {
-        // let errorsArray = this.props.errors;
+        // let errorsArray; //= this.props.errors;
         // if (this.props.errors) { //check if there are any errors
         //     errorsArray = this.props.errors.map(error => <p> {error} </p>)
         // };
@@ -48,38 +47,42 @@ class SignupForm extends React.Component {
                 <h1 className='form-header'>Welcome</h1>
                 <h3>Create an account.</h3>
                 <form className='session-form'>
-                    <label>Email address
-                            <input
+
+                    <div id="floatLabel" className="float-label">
+                        <label htmlFor="floatEmail">Email address</label>
+                        <input id="floatEmail"
                             type="text"
                             value={this.props.email}
                             readOnly />
-                    </label>
+                    </div>
+                    
                     <label>Confirm email
                             <input
                             type="text"
-                            value={this.state.signup.email}
+                            value={this.state.email}
                             // onChange={this.confirmEmail} 
                             onChange={this.update('email')} />
                     </label>
-                    {/* <p>{this.state.error}</p> */}
+                    <p>{this.confirmEmail()}</p>
                     <label>First Name
                             <input
                             type="text"
-                            value={this.state.signup.fname}
+                            value={this.state.fname}
                             onChange={this.update('fname')} />
                     </label>
                     <label>Last Name
                             <input
                             type="text"
-                            value={this.state.signup.lname}
+                            value={this.state.lname}
                             onChange={this.update('lname')} />
                     </label>
                     <label>Password
                             <input
                             type="password"
-                            value={this.state.signup.password}
+                            value={this.state.password}
                             onChange={this.update('password')} />
                     </label>
+                    <p>{this.validPW()}</p>
                     <button onClick={this.handleClick}>Sign Up</button>
                 </form>
             </div>
